c3ccce480319cf93270253595a63c1595dcb4bfd
aayus <aayush01@live.com>
2020-05-20 00:08:18 -0700

    Update ex3.m
    test

--- machine-learning-ex3/ex3/ex3.m
+++ machine-learning-ex3/ex3/ex3.m
@@ -14,7 +14,7 @@
 %
 %  For this exercise, you will not need to change any code in this file,
 %  or any other files other than those mentioned above.
-%
+% test for github to double check that it works
 
 %% Initialization
 clear ; close all; clc
